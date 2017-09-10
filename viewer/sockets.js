fcfb69b889a1a1ea6b8e50568d2a73e7d15b6419
nathanhleung <nathanhleung@users.noreply.github.com>
2017-09-10 01:19:22 -0400

    socket.io file

--- viewer/sockets.js
+++ viewer/sockets.js
@@ -1,4 +1,14 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://35.190.181.97');
 socket.on('data', function (data) {
+  /**
+   * Data is in this format:
+   * { clientPosition: [ 2.1, 1.1 ],
+   *   beaconData: [
+   *    { position: [ 0, 3 ], radius: '5' },
+   *    { position: [ 0, 0 ], radius: '2' },
+   *    { position: [ 3, 0 ], radius: '7' }
+   *  ]
+   * }
+   */
   console.log(data);
 });
