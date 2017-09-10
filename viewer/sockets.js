c60521e11af92cb5711f49ea51ea120c10a49227
nathanhleung <nathanhleung@users.noreply.github.com>
2017-09-10 02:41:30 -0400

    frontend fix

--- viewer/sockets.js
+++ viewer/sockets.js
@@ -2,15 +2,17 @@ var socket = io.connect('http://35.190.181.97');
 socket.on('data', function (data) {
   /**
    * Data is in this format:
-   * { clientPosition: [ 2.1, 1.1 ],
+   * {
+   * data: { clientPosition: [ 2.1, 1.1 ],
    *   beaconData: [
    *    { position: [ 0, 3 ], radius: '5' },
    *    { position: [ 0, 0 ], radius: '2' },
    *    { position: [ 3, 0 ], radius: '7' }
    *  ]
    * }
+   * }
    */
   console.log(data);
 
-    plotIt(data.clientPosition[0],data.clientPosition[1],data.beaconData[0].position[0],data.beaconData[0].position[1],data.beaconData[0].radius,data.beaconData[1].position[0],data.beaconData[1].position[1],data.beaconData[1].radius,data.beaconData[2].position[0],data.beaconData[2].position[1],data.beaconData[0].radius)
+    plotIt(data.data.clientPosition[0],data.clientPosition[1],data.beaconData[0].position[0],data.beaconData[0].position[1],data.beaconData[0].radius,data.beaconData[1].position[0],data.beaconData[1].position[1],data.beaconData[1].radius,data.beaconData[2].position[0],data.beaconData[2].position[1],data.beaconData[0].radius)
 });
