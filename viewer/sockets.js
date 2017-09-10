583862a6639efa216866844a0dee707345c4be54
nathanhleung <nathanhleung@users.noreply.github.com>
2017-09-10 02:43:35 -0400

    routes

--- viewer/sockets.js
+++ viewer/sockets.js
@@ -1,5 +1,5 @@
 var socket = io.connect('http://35.190.181.97');
-socket.on('data', function (data) {
+socket.on('data', function ({ data }) {
   /**
    * Data is in this format:
    * {
@@ -12,7 +12,7 @@ socket.on('data', function (data) {
    * }
    * }
    */
-  console.log(data);
+   console.log(data);
 
-    plotIt(data.data.clientPosition[0],data.clientPosition[1],data.beaconData[0].position[0],data.beaconData[0].position[1],data.beaconData[0].radius,data.beaconData[1].position[0],data.beaconData[1].position[1],data.beaconData[1].radius,data.beaconData[2].position[0],data.beaconData[2].position[1],data.beaconData[0].radius)
+    plotIt(data.clientPosition[0],data.clientPosition[1],data.beaconData[0].position[0],data.beaconData[0].position[1],data.beaconData[0].radius,data.beaconData[1].position[0],data.beaconData[1].position[1],data.beaconData[1].radius,data.beaconData[2].position[0],data.beaconData[2].position[1],data.beaconData[0].radius)
 });
