9093ab641dbd7fd975d3f805be53e80a3cd08736
nathanhleung <nathanhleung@users.noreply.github.com>
2017-09-10 03:44:40 -0400

    viewer

--- viewer/sockets.js
+++ viewer/sockets.js
@@ -20,16 +20,16 @@ socket.on('data', function ({ data }) {
    ]);
 
     plotIt([
-      data.clientPosition[0] / 2,
-      data.clientPosition[1] / 2,
-      data.beaconData[0].position[0] / 2,
-      data.beaconData[0].position[1] / 2,
-      data.beaconData[0].radius * 2,
-      data.beaconData[1].position[0] / 2,
-      data.beaconData[1].position[1] / 2,
-      data.beaconData[1].radius * 2,
-      data.beaconData[2].position[0] / 2,
-      data.beaconData[2].position[1] / 2,
-      data.beaconData[0].radius * 2,
+      data.clientPosition[0] * 0.5,
+      data.clientPosition[1] * 0.5,
+      data.beaconData[0].position[0],
+      data.beaconData[0].position[1],
+      data.beaconData[0].radius,
+      data.beaconData[1].position[0],
+      data.beaconData[1].position[1],
+      data.beaconData[1].radius,
+      data.beaconData[2].position[0],
+      data.beaconData[2].position[1],
+      data.beaconData[0].radius,
     ]);
 });
